31f2ba313191fd0292c537061a504d6146ee61ac
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-08-14 12:50:22 +0300

    Initial commit

---
+++ src/test/java/com/nesmy/daysinyearpassedbot/DaysInYearPassedBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nesmy.daysinyearpassedbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaysInYearPassedBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
