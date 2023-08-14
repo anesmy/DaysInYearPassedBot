cac8a4f3ff05eaf819850f659a19c1f4121a697e
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-08-14 15:57:54 +0300

    Implement DaysInYearPassedBot and BotInitializer classes

--- src/main/java/com/nesmy/daysinyearpassedbot/config/BotInitializer.java
+++ src/main/java/com/nesmy/daysinyearpassedbot/config/BotInitializer.java
@@ -1,2 +1,32 @@
-package com.nesmy.daysinyearpassedbot.config;public class BotInitializer {
+package com.nesmy.daysinyearpassedbot.config;
+
+import com.nesmy.daysinyearpassedbot.service.DaysInYearPassedBot;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Slf4j
+@Component
+public class BotInitializer {
+
+    @Autowired
+    DaysInYearPassedBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException{
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+            log.info("Registering bot...");
+        } catch (TelegramApiRequestException e) {
+            log.error("Failed to register bot(check internet connection / bot token or make sure only one instance of bot is running).", e);
+        }
+        log.info("Telegram bot is ready to accept updates from user......");
+    }
 }
--- src/main/java/com/nesmy/daysinyearpassedbot/service/DaysInYearPassedBot.java
+++ src/main/java/com/nesmy/daysinyearpassedbot/service/DaysInYearPassedBot.java
@@ -1,2 +1,68 @@
-package com.nesmy.daysinyearpassedbot.service;public class DaysInYearPassedBot {
+package com.nesmy.daysinyearpassedbot.service;
+
+import com.nesmy.daysinyearpassedbot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Slf4j
+@Component
+public class DaysInYearPassedBot extends TelegramLongPollingBot{
+
+    final BotConfig config;
+
+    public DaysInYearPassedBot(BotConfig config){
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start" -> startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                case "/stop" -> stopCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                default -> sendMessage(chatId, "Command was not recognized.");
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String name){
+        String answer = "Hi, " + name + "!" +
+                "\nFrom now on, you will be notified of how many days have already passed this year (in percentage) every morning at 9:00. " +
+                "\nYou can stop receiving messages by pressing /stop";
+        sendMessage(chatId, answer);
+    }
+
+    private void stopCommandReceived(long chatId, String name){
+        String answer = "Hi, " + name + "!" +
+                "\nFrom now on, you will not be notified of how many days have already passed this year (in percentage) every morning at 9:00. " +
+                "\nYou can start receiving messages by pressing /start";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend){
+        SendMessage message = new SendMessage(String.valueOf(chatId), textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e){
+            log.error(e.getMessage());
+        }
+    }
 }
